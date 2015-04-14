a4deaad9fd74e4b3f9482dd149ba24d613255a1f
sduxz <zxcvb1345qq@163.com>
2015-04-14 17:48:57 +0800

    add pic

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -60,17 +60,38 @@ namespace FingerDroid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int imageView1 = 2131034113;
-			
 			// aapt resource value: 0x7f050000
-			public const int relativeLayout1 = 2131034112;
+			public const int baselayout = 2131034112;
 			
-			// aapt resource value: 0x7f050002
-			public const int switch1 = 2131034114;
+			// aapt resource value: 0x7f05000a
+			public const int button1 = 2131034122;
 			
 			// aapt resource value: 0x7f050003
-			public const int textView1 = 2131034115;
+			public const int enrollbtn = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int imageView1 = 2131034119;
+			
+			// aapt resource value: 0x7f050001
+			public const int linearLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050006
+			public const int relativeLayout1 = 2131034118;
+			
+			// aapt resource value: 0x7f050005
+			public const int resulttext = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int statustext = 2131034116;
+			
+			// aapt resource value: 0x7f050008
+			public const int switch1 = 2131034120;
+			
+			// aapt resource value: 0x7f050002
+			public const int takepicb = 2131034114;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView1 = 2131034121;
 			
 			static Id()
 			{
